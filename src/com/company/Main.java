e861478fdff1bd20a595bc9d40fbf12015568067
jisookwon <jinskwon@hotmail.com>
2019-04-24 10:48:55 -0400

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        string letters = keyboard.nextLine();
+        int length=1;
+        for (int i=0; i<length; i++){
+        if (letters[i]==""){
+            length=i;
+        }
+
+            }
+
+
+
+    }
+}
